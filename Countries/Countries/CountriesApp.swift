1005fa3a48e7cdd4dfa89fee2bcb0dcdd129c47b
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-11-27 20:42:16 +0200

    initial commit

---
+++ Countries/Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Amr Hesham on 27/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
